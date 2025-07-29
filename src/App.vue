8d26ac492b9fa8117c52f80e4247c2acb1478779
Nesodev <ever.avendano@unmsm.edu.pe>
2025-07-28 21:51:37 -0500

    redefine structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="app">
+    <div class="head">
+      <div class="logo">
+        <img src="./assets/logo-nutrimente.png" alt="">
+        <h2>Nutrimente</h2>
+      </div>
+      <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+      <div></div>
+    </div>
+      <router-view />
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: #F3F1EA;
+  display: flex;
+  flex-direction: column;
 }
 
-nav {
-  padding: 30px;
+.head {
+  position:fixed;
+  top: 0;
+  width: 100%;
+  height: 75px;
+  background: black;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="hero-section">
+        <img src="../assets/hero.png" alt="">
+    </div>
+</template>
+
+<style scoped>
+.hero-section {
+    width: 100%;
+    height: 100%;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+</style>
